5cfe16e47b4579957eea3344747d4c47200f6bff
Anya Krivosheeva <anyakrivosheeva98@gmail.com>
2026-01-30 15:25:30 +0300

    middleware

--- redux-app/src/app/store.js
+++ redux-app/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import usersReducer from '../features/users/usersSlice';
 import postsReducer from '../features/posts/postsSlice';
+import { loggerMiddleware } from './loggerMiddleware'; 
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         users: usersReducer,
         posts: postsReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware),
 });
